73018b4f42b763a34662b8c24e2bdca73c22f553
Nicolas-Chasteler <85590455+Nicolas-Chasteler@users.noreply.github.com>
2024-09-14 00:47:08 -0500

    Add logger and fix filename
    Adding logging to postgres server and fixing "#main.py" to "main.py"

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 # main.py
 import os
 import discord
+import logging
+import utils
 from discord.ext import commands
 
+logger = PostgresLogger.initialize_logger()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-
-bot = discord.Client()
 
 
 class DiscordBot(discord.Client):
     async def on_ready(self):
-        #todo logging
+        logger.info(f"Logged in as {self.user}. ID: {self.user.id}")
 
     async def on_message(message):
         #todo send message to specified channel, maybe respond to initial message after some time?
 
 
-if __name__ = "__main__":
+def main():
+    bot = DiscordBot()
     bot.run(TOKEN)
+
+if __name__ = "__main__":
+    main()
+
